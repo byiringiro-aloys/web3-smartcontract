c666c67b568c71f7dd18772c1432d30b8d31954d
byiringiro-aloys <byiringiroaloys8@gmail.com>
2026-04-27 09:05:08 +0200

    Delete default files

--- Functions_based/contracts/scopes.sol
+++
@@ -1,9 +0,0 @@
-//SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.2;
-
-contract ScopeResolve{
-    function add(uint a, uint b)public pure returns (uint,bool){
-        return (a+b,true);
-    }
-}
